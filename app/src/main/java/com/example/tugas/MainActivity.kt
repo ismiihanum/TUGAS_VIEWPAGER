f373296f25fd9a69931fab72b993f3f0a179eaf5
ismiihanum <ismihanum05@gmail.com>
2019-03-19 00:09:58 +0700

    adding profil

---
+++ app/src/main/java/com/example/tugas/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tugas
+
+
+import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = FragmentPagerItemAdapter(
+            supportFragmentManager, FragmentPagerItems.with(this)
+                .add("First", First::class.java)
+                .add("second", second::class.java)
+                .add("third", third::class.java)
+                .add("fourth", fourth::class.java)
+
+                .create()
+        )
+
+        val viewPager = findViewById(R.id.viewpager) as ViewPager
+        viewPager.adapter = adapter
+
+        val viewPagerTab = findViewById(R.id.viewpagertab) as SmartTabLayout
+        viewPagerTab.setViewPager(viewPager)
+    }
+}
